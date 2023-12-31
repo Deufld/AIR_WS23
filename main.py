f9566c332b95ac821199c097a7ec77efa7004644
Florian Deutschmann <florian.d2110@gmail.com>
2023-12-31 14:24:07 +0100

    added possibility to send queries

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import io_util
 import sentiment
 import preprocessing
+import pandas as pd
 
 negative_preprocessed_data = None
 neutral_preprocessed_data = None
@@ -12,7 +13,8 @@ def print_help():
     print("possible actions".center(80, '='))
     print("[1] perform sentiment classification and preprocessing on dataset")
     print("[2] load preprocessed and classified data from csv-files")
-    print("[3] quit program")
+    print("[3] send query")
+    print("[4] quit program")
     print("".center(80, '='))
 
 
@@ -42,12 +44,42 @@ def load_sentiment_classification():
         negative_preprocessed_data = io_util.read_csv('data/negative_dataset.csv', [], False)
         neutral_preprocessed_data = io_util.read_csv('data/neutral_dataset.csv', [], False)
         positive_preprocessed_data = io_util.read_csv('data/positive_dataset.csv', [], False)
-
-        print(negative_preprocessed_data)
-        print(neutral_preprocessed_data)
-        print(str(len(negative_preprocessed_data) + len(neutral_preprocessed_data) + len(positive_preprocessed_data)))
     except FileNotFoundError:
         print("Files to load sentiment classification do not exist!")
+
+def send_query():
+    global negative_preprocessed_data, neutral_preprocessed_data, positive_preprocessed_data
+
+    if negative_preprocessed_data is None or neutral_preprocessed_data is None or positive_preprocessed_data is None:
+        print("Cannot perform query, dataset has not yet been loaded / classified!")
+        return
+
+    ir_method = input("Enter IR-Method of choice [bm25;tfidf]: ")
+    if ir_method not in ['bm25', 'tfidf']:
+        print("Invalid/not supported IR-Method!")
+        return
+
+    sentiment_of_choice = input("Enter sentiment(s) of choice (separated by semicolon) [negative, neutral, positive]: ")
+    sentiments = sentiment_of_choice.split(";")
+
+    for s in sentiments:
+        if s not in ['positive', 'neutral', 'negative']:
+            print("Invalid / not supported sentiment was entered!")
+            return
+
+    query_str = input("Enter query: ")
+    query_as_df = pd.DataFrame(data=[query_str], columns=['text'])
+    preprocessed_query = preprocessing.preprocess_data(query_as_df)
+
+    print(preprocessed_query)
+
+    if ir_method == 'bm25':
+        # TODO: add bm25 IR here
+        pass
+    elif ir_method == 'tfidf':
+        # TODO add tfidf IR here
+        pass
+
 
 
 print("Program started".center(80, "="))
@@ -63,6 +95,8 @@ while True:
     elif operation == 2:
         load_sentiment_classification()
     elif operation == 3:
+        send_query()
+    elif operation == 4:
         break
     else:
         print("Invalid operation!")
